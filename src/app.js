08fb776161564202211c8c8aa736c4cdd988478c
Anna Sobanska <ania.webdev@gmail.com>
2021-04-07 13:42:06 +0200

    add style.css to project

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import ListWrapper from './components/ListWrapper/ListWrapper';
+import './index.css';
 
-//functional component, use {} only with return
 const App = () => (
-    <div className="wrapper">
-        <h1 className="mainHeader">Hello world!</h1>
-        <h2 className="secondaryHeader">Hello weduweb!</h2>
+    <div>
+        <ListWrapper />
     </div>
 );
 
