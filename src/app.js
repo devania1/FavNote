13649106c01ea5729b0466359c2d3f655ee3436f
Anna Sobanska <ania.webdev@gmail.com>
2021-04-07 10:25:38 +0200

    add app.js and default export

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+//functional component, use {} only with return
+const App = () => (
+    <div className="wrapper">
+        <h1 className="mainHeader">Hello world!</h1>
+        <h2 className="secondaryHeader">Hello weduweb!</h2>
+    </div>
+);
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
 import React from 'react'; //to create react components
 import ReactDOM from 'react-dom';//render to html
-
-//functional component, use {} only with return
-const App = () => (
-    <div className="wrapper">
-        <h1 className="mainHeader">Hello world!</h1>
-        <h2 className="secondaryHeader">Hello weduweb!</h2>
-    </div>
-); 
+import App from './app';
 
 
-ReactDOM.render(
-    //rendered element
-    <App />,
-    //target
-    document.getElementById('root')
+ReactDOM.render(<App />, document.getElementById('root')
 );
