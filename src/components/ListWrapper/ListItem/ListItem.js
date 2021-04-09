d93ee355f1eaa0f31fefd624707f1d305e9e05d2
Anna Sobanska <ania.webdev@gmail.com>
2021-04-09 11:07:39 +0200

    create dynamic list - props, map

--- src/components/ListWrapper/ListItem/ListItem.js
+++ src/components/ListWrapper/ListItem/ListItem.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './ListItem.css';
-import danAbramovImage from '../../../assets/images/danabramov.jpg'
 
-const ListItem = () => (
+const ListItem = (props) => (
     <li className="listItem__wrapper">
-        <img src={danAbramovImage} className="listItem__image"/>
+        <img 
+            src={props.image} className="listItem__image"
+            alt={props.name}/>
         <div>
-            <h2 className="listItem__name">Dan Abramov</h2>
-            <p className="listItem__description">It is a good idea to think of your PC as an office. It stores files, programs, pictures. This can be compared to an actual office's files, machines and decorations.</p>
-            <button className="listItem__button">visit twitter page</button>
+            <h2 className="listItem__name">{props.name}</h2>
+            <p className="listItem__description">{props.description}</p>
+            <a href={props.link} className="listItem__button">visit twitter page</a>
         </div>
     </li>
 );
