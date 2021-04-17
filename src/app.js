e96e4ab3a2e3f3d1467c2b90354f55b24ac75217
Anna Sobanska <ania.webdev@gmail.com>
2021-04-18 00:23:22 +0200

    add state component

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ListWrapper from './components/ListWrapper/ListWrapper';
+import MyComponent from './components/myComponent/myComponent';
 import './index.css';
 
 const App = () => (
     <div>
+        <MyComponent />
         <ListWrapper />
     </div>
 );
---
+++ src/components/myComponent/myComponent.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class MyComponent extends React.Component {
+    state = {
+        text: 'test state',
+    }
+    
+    handleChange = () => console.log('change');
+    
+    render() {
+        return (
+        <>
+            <input placeholder="your text"
+            onChange={this.handleChange} />
+            <h1>{this.state.text}</h1>
+        </>
+        )
+    }
+}
+
+export default MyComponent;
