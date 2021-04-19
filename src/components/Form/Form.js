fa4f207c5db9674bd8cf6d04e874afcd60682a86
Anna Sobanska <ania.webdev@gmail.com>
2021-04-19 22:22:38 +0200

    create form

---
+++ src/components/Form/Form.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Form = ({submitFn}) => (
+   <form onSubmit={submitFn}>
+       <input placeholder="name" name="name" />
+       <input placeholder="link" name="link" />
+       <input placeholder="image" name="image" />
+       <textarea placeholder="description" name="description" />
+       <button type="submit">Add new item</button>
+   </form> 
+)
+
+export default Form;
