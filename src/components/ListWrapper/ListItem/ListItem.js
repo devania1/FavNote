7146030060e0f605c133132c69274e7272f027cb
Anna Sobanska <ania.webdev@gmail.com>
2021-04-22 08:57:54 +0200

    add css modules

--- src/components/ListWrapper/ListItem/ListItem.js
+++ src/components/ListWrapper/ListItem/ListItem.js
@@ -1,35 +1,43 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './ListItem.css';
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./ListItem.module.scss";
 
-const ListItem = ({name, description, image, twitterLink}) => (
-    <li className="listItem__wrapper">
-        <img 
-            src={image} 
-            className="listItem__image"
-            alt={name}
+const ListItem = ({ image, name, description, twitterLink }) => {
+
+    const ImageTag = image ? 'img' : 'div';
+
+    return (  
+    <li className={styles.wrapper}>
+      <ImageTag 
+        src={image} 
+        className={image ? styles.image : styles.imageNone} 
+        alt={name} 
         />
-        <div>
-            <h2 className="listItem__name">{name}</h2>
-            <p className="listItem__description">{description}</p>
-            <a href={twitterLink} 
-               target="_blank"
-               className="listItem__button">
-               visit twitter page
-            </a>
-        </div>
+      <div>
+        <h2 className={styles.name}>{name}</h2>
+        <p className={styles.description}>{description}</p>
+        <a 
+          href={twitterLink} 
+          target="_blank" 
+          className={styles.button}
+          rel="noopener noreferrer">
+          visit twitter page
+        </a>
+      </div>
     </li>
-);
+    )
+  };
 
 ListItem.propTypes = {
-    image: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    description: PropTypes.string,
-    twitterLink: PropTypes.string.isRequired,
+  image: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  twitterLink: PropTypes.string.isRequired
 };
 
 ListItem.defaultProps = {
-    description: 'One of the React creators',
+  image: null,
+  description: "One of the React creators",
 };
 
 export default ListItem;
