a671f4733ee5279f55503c35b7148fbfcbe84ebd
Anna Sobanska <ania.webdev@gmail.com>
2021-04-27 00:18:21 +0200

    add views

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import ListWrapper from '../../components/List/List';
 import './index.css';
-import Form from '../../components/Form/Form.js'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import ArticlesView from '../ArticlesView/ArticlesView';
+import TwittersView from '../TwittersView/TwittersView';
+import NotesView from '../NotesView/NotesView';
+import Navigation from '../../components/Navigation/Navigation';
 
 const initialStateItems = [
 {
@@ -54,12 +57,17 @@ class Root extends React.Component {
 
     render() {
         return (
-            <div>
-                <ListWrapper 
-                    items={this.state.items} 
-                />
-                <Form submitFn={this.addItem} />
-            </div>
+            <BrowserRouter>
+                <>
+                <Navigation />
+                <h1>Hello world!</h1>
+                <Switch>
+                <Route exact path="/" component={TwittersView} />
+                <Route path="/articles" component={ArticlesView} />
+                <Route path="/notes" component={NotesView} />
+                </Switch>
+                </>
+            </BrowserRouter>
             )
     }  
 }
