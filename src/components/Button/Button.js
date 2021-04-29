7886096d2aa8895bca4deb5c021ab75a9649f833
Anna Sobanska <ania.webdev@gmail.com>
2021-04-29 11:32:06 +0200

    add secondary to button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import styles from './Button.module.scss';
 
-const Button = ({children, href}) => (
+const Button = ({children, href, secondary}) => {
+
+    const buttonClass = secondary ? styles.secondary : styles.button;
+
+    return (
     <>
     { 
         href ? (
             <a
                 href={href}
                 target="_blank" 
-                className={styles.button}
+                className={buttonClass}
                 rel="noopener noreferrer"
             >
                 {children}
             </a >
           ) : (    
-            <button className={styles.button}>
+            <button className={buttonClass}>
                 {children}
            </button>
         )
     }
     </>
-);
+   )
+
+};
 
 export default Button;
